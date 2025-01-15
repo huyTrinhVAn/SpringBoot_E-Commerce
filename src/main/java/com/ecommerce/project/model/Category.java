6f2e0a64e500dca93523a2a9671a0f1a36da83d9
huyTrinhVAn <trinhvanhuy11a9@gmail.com>
2025-01-15 16:28:13 +1100

    JPA and H2 database integrate

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,13 +1,30 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.*;
+
+@Entity(name = "categories")
 public class Category {
-
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
+//    @Version
+//    private Integer version; // Thêm để kiểm soát phiên bản của thực thể
+
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public Category() {
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
     }
 
@@ -17,13 +34,5 @@ public class Category {
 
     public void setCategoryId(Long categoryId) {
         this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
     }
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,34 +1,59 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @Service
-public class CategoryServiceImpl implements CategoryService {
-    private List<Category> categories = new ArrayList<>();
-    private long nextId = 1L;
+public class CategoryServiceImpl implements CategoryService{
+    //private List<Category> categories = new ArrayList<>();
+//    private Long nextId = 1L;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+//        category.setCategoryId(nextId++);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream()
-                .filter(c -> c.getCategoryId().equals(categoryId)).
-                findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND , "Resource not found"));
+        categoryRepository.delete(category);
+        return "Category with categoryId:" + categoryId + " deleted";
+        // Another method
+//        if (!categoryRepository.existsById(categoryId)) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found");
+//        }
+//        categoryRepository.deleteById(categoryId);
+//        return "Category with categoryId: " + categoryId + " deleted";
 
-        categories.remove(category);
-        return "Category with categoryId: " + categoryId + " deleted";
+    }
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> savedCategoryOptional = categoryRepository.findById(categoryId);
+
+
+        Category savedCategory = savedCategoryOptional.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+
+        category.setCategoryId(categoryId);
+
+        savedCategory = categoryRepository.save(category);
+        return savedCategory;
+
     }
 }
