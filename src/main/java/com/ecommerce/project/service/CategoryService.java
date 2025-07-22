46f1aaf356a061b59b09e32d9eab4749b934f0c9
huyTrinhVAn <trinhvanhuy11a9@gmail.com>
2025-07-22 22:12:45 +1000

    feat(app): adding sorting for Page and make validation reponse more standard

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -7,10 +7,11 @@ import com.ecommerce.project.payload.CategoryResponse;
 import java.util.List;
 
 public interface CategoryService {
-    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
+    CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy ,String sortOrder);
     CategoryDTO createCategory(CategoryDTO categoryDTO);
 
     CategoryDTO deleteCategory(Long categoryId);
 
     CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
+
 }
